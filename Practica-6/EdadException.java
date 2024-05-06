b8cfb7e7337932740bf5cad4fb194d90968792b6
EdgarR <edo_skate@hotmail.com>
2024-05-06 08:03:05 -0500

    ultima actualizacion Practica-6

---
+++ Practica-6/EdadException.java
@@ -0,0 +1,21 @@
+public class EdadException extends Exception{
+	public EdadException(){
+
+	}
+
+	public EdadException (String mensaje){
+		super(mensaje);
+	}
+
+		public EdadException(int edad) throws EdadException{
+			this.validarEdad(edad);
+		}
+
+		public void validarEdad(int edad) throws EdadException{
+			if(!(edad >= 18 && edad >= 45)){
+				//System.out.println("La edad capturada no pertenece al rango permitido (18 a 45 años), favor de capturar:");
+				throw new EdadException("Error de edad.");
+			}
+		}
+
+}
